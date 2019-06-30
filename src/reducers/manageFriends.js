9c2afc52ccea5f4d9dd8af68cf68b414fc2abd8d
github username <github email address>
2019-06-29 22:15:11 -0400

    wip - testing conditions

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+    return {}
+    case 'REMOVE_FRIEND':
+    return {}
+
+    default:
+    return state;
+  }
 }
