e3ff77907ae26ab1c04ed63b866191b83625b71a
github username <github email address>
2019-06-30 12:16:14 -0400

    use findindex and spread operator the resolved the last condition- remove friend.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,11 +1,22 @@
 export function manageFriends(state, action){
   switch(action.type) {
     case 'ADD_FRIEND':
-    return {}
+      return(
+        // Object.assign({}, state, friends: [Object.assign({}, state.friends, action.friend)])
+        {...state, friends: [...state.friends, action.friend]}
+      )
+
     case 'REMOVE_FRIEND':
-    return {}
+
+      let index = state.friends.findIndex(friend => friend.id === action.id)
+      return (
+        {...state, friends: [
+          ...state.friends.slice(0, index), ...state.friends.slice(index + 1)
+        ]
+      }
+      )
 
     default:
-    return state;
+      return state;
   }
 }
