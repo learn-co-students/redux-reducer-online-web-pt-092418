0faf7bf3fabad4fdc9d253d3266bbbb58760ca3a
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-08-17 18:08:12 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
+// let state = {count: 0}
+
 export function manageFriends(state, action){
+    
+
+    switch(action.type){
+
+        case 'ADD_FRIEND':
+            //  state.friends.push(action.friend)
+            //  let newArray = state.friends
+            // return {friends: newArray}
+
+
+            return Object.assign({}, state, {
+                friends: [...state.friends, action.friend]
+            });
+        case 'REMOVE_FRIEND':
+            return Object.assign({}, state, {
+                friends: state.friends.filter(friend => friend.id !== action.id)
+            });
+        default:
+            return state;
+    }
 }
+
